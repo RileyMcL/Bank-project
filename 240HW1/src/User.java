4aee6c919ddf18898f51b617795d4ca28ca7618f
rildom <41808672+rildom@users.noreply.github.com>
2019-10-15 16:08:48 -0400

    banking program
    s

---
+++ 240HW1/src/User.java
@@ -0,0 +1,27 @@
+
+public class User {
+
+	protected String fName;
+	protected String lName;
+	protected String bDay;
+	protected int licNum;
+	protected String job;
+	protected String address;
+
+	User(String fName, String lName, String bDay, int licNum, String job, String address) {
+		this.fName = fName;
+		this.lName = lName;
+		this.bDay = bDay;
+		this.licNum = licNum;
+		this.job = job;
+		this.address = address;
+
+	}
+	
+	
+	//protected void openCAccount(String fName, int licNum, ) {
+		
+	//}
+	
+
+}
